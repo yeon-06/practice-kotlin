9f8cdda78362974a2c230ab74308ca2a3d2769c3
yeon-06 <yeonlog06@gmail.com>
2023-02-13 10:46:45 +0900

    feat: Hello Kotlin!

---
+++ practice/src/main/kotlin/Hello.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    println("Hello Kotlin!")
+}
+
+/* 코틀린 기초 문법
+- 파라미터 이름 뒤에 파라미터의 타입을 쓴다
+- 함수를 최상위 수준에 정의할 수 있다 (클래스 안에 함수를 넣을 필요가 없다)
+- System.out.println 대신 println 같이 표준 자바 라이브러리 함수를 간결하게 사용할 수 있게 감싼 래퍼 제공
+ */
