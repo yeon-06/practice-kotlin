5e0570d2abba19519d3e2217c3f25a7c05bb653c
yeon-06 <yeonlog06@gmail.com>
2023-02-25 22:58:54 +0900

    feat: data class 예제

--- practice/src/main/kotlin/Basic3.kt
+++ practice/src/main/kotlin/Basic3.kt
@@ -5,6 +5,19 @@ class Number {
     fun add(addValue: Int) {
         value += addValue
     }
+
+    override fun toString(): String {
+        return String.format("%s(value=%d)", javaClass.name, value)
+    }
+}
+
+data class NumberData(
+        var value: Int = 0
+) {
+
+    fun add(addValue: Int) {
+        value += addValue
+    }
 }
 
 fun main() {
@@ -12,4 +25,18 @@ fun main() {
 //    number.value = 10 -> setter가 private라 오류가 발생한다
     number.add(10)
     println(number.value)
+
+    // 재정의한 toString 호출
+    println(number)
+
+    // data class 로 정의하면 toString 자동으로 재정의
+    val numberData = NumberData()
+    println(numberData)
+
+    // data class 로 정의하면 equals, hashCode 자동으로 재정의
+    val numberData2 = NumberData(10)
+    numberData.add(10)
+    println("numberData: $numberData")
+    println("numberData2: $numberData2")
+    println(numberData == numberData2)
 }
