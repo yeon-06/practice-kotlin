ce93f7d7076d9b8e11a54faf43c3e865fa90d106
yeon-06 <yeonlog06@gmail.com>
2023-02-28 11:16:09 +0900

    feat: 콘솔 출력용 view 구현

---
+++ racingcar/src/main/kotlin/view/OutputView.kt
@@ -0,0 +1,26 @@
+package view
+
+import domain.Cars
+
+object OutputView {
+
+    fun printStartSentence() {
+        println("실행 결과")
+    }
+
+    fun printStatus(cars: Cars) {
+        cars.value.forEach {
+            println(String.format("%s : %s", it.name, drawStatusBar(it.location)))
+        }
+    }
+
+    private fun drawStatusBar(value: Int): String {
+        return "-".repeat(value)
+    }
+
+    fun printResult(names: List<String>) {
+        println(
+                String.format("%s가 최종 우승했습니다.", names.joinToString(separator = ", "))
+        )
+    }
+}
