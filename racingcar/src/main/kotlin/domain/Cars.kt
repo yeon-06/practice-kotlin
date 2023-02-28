486631a0d87d210779fcac304f124d8b11c39d98
yeon-06 <yeonlog06@gmail.com>
2023-02-28 10:57:57 +0900

    feat: 자동차 목록에서 이름 중복 검증

---
+++ racingcar/src/main/kotlin/domain/Cars.kt
@@ -0,0 +1,22 @@
+package domain
+
+import java.util.stream.Collectors
+
+class Cars(names: List<String>) {
+
+    private val value: List<Car>
+
+    init {
+        validateNames(names)
+        value = names.stream()
+                .map { Car(it) }
+                .collect(Collectors.toList())
+    }
+
+    private fun validateNames(names: List<String>) {
+        val nameSet = setOf(names)
+        if(nameSet.size != names.size) {
+            throw IllegalArgumentException("자동차 이름 목록에 중복이 존재합니다.")
+        }
+    }
+}
