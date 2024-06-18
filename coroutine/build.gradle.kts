44344c44f162ba1b634a41085eec558e79c3ca5c
yeon <yeon@woowahan.com>
2024-06-18 22:46:58 +0900

    chore: 코루틴 테스트 의존성 추가 docs: 블로그 링크 추가

--- coroutine/build.gradle.kts
+++ coroutine/build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 }
 
